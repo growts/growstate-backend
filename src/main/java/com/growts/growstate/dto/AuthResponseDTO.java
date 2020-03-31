e5d30d120ec32a5e779f06fc5ff24ef120d09685
Nicolas Estevez <nicocac@gmail.com>
2020-03-31 08:13:15 -0300

    modificaciones iniciales

---
+++ src/main/java/com/growts/growstate/dto/AuthResponseDTO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.growts.growstate.dto;
+
+/**
+ *
+ * @author etorrielli
+ */
+public class AuthResponseDTO {
+
+    private String token;
+    private String user;
+    private String rol;
+
+    public AuthResponseDTO(){
+
+    }
+
+    public AuthResponseDTO(String token, String user, String rol){
+        this.token = token;
+        this.user = user;
+        this.rol = rol;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+}
---
+++ src/main/java/com/growts/growstate/exception/ServiceCallException.java
@@ -0,0 +1,29 @@
+package com.growts.growstate.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class ServiceCallException extends RuntimeException {
+
+    public ServiceCallException(String servicio, String... searchParamsMap) {
+        super(ServiceCallException.generateMessage(searchParamsMap[0]));
+    }
+
+    private static String generateMessage(String message) {
+        return message;
+    }
+
+    private static <K, V> Map<K, V> toMap(
+            Class<K> keyType, Class<V> valueType, Object... entries) {
+        if (entries.length % 2 == 1) {
+            throw new IllegalArgumentException("Invalid entries");
+        }
+        return IntStream.range(0, entries.length / 2).map(i -> i * 2)
+                .collect(HashMap::new,
+                        (m, i) -> m.put(keyType.cast(entries[i]), valueType.cast(entries[i + 1])),
+                        Map::putAll);
+    }
+
+}
+
