fbed906ccfa85787c2d6a34d78be3d37e0f90783
Nicolas Estevez <nicocac@gmail.com>
2020-04-03 17:49:28 -0300

    modificaciones login

--- src/main/java/com/growts/growstate/controller/UserController.java
+++ src/main/java/com/growts/growstate/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.growts.growstate.controller;
 
+import com.growts.growstate.dto.Response;
 import com.growts.growstate.exception.ResourceNotFoundException;
 import com.growts.growstate.payload.UserIdentityAvailability;
 import com.growts.growstate.payload.UserProfile;
@@ -11,11 +12,13 @@ import com.growts.growstate.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/usuario")
 public class UserController {
 
     @Autowired
@@ -50,6 +53,11 @@ public class UserController {
         UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
 
         return userProfile;
+    }
+
+    @GetMapping(value="/isLogged")
+    public ResponseEntity<String> isLogged() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
 }
